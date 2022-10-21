99e342e7ad82c51f4d90bbb0113ad22c67fd2466
Irina <irakali86@gmail.com>
2022-10-21 19:48:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int client_account = 100;
+        int deposit_amount = 1500;
+        int bonus = deposit_amount / 100;
+        if (deposit_amount > 1000) {System.out.println(bonus = deposit_amount /  100); }
+        else {System.out.println(bonus = 0);}
+        System.out.println(client_account + deposit_amount + bonus);
+    }
+}
