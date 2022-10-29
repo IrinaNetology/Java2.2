b43da656ad758ca244d0b302e8bf629190fce4b4
Irina <irakali86@gmail.com>
2022-10-29 11:15:34 +0300

    formating

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int client_account = 100;
-        int deposit_amount = 1500;
-        int bonus = deposit_amount / 100;
-        if (deposit_amount > 1000) {System.out.println(bonus = deposit_amount /  100); }
-        else {System.out.println(bonus = 0);}
-        System.out.println(client_account + deposit_amount + bonus);
+        int clientAccount = 100;
+        int depositAmount = 1500;
+        int bonus = depositAmount / 100;
+        if (depositAmount > 1000) {
+            System.out.println(bonus = depositAmount / 100);
+        } else {
+            System.out.println(bonus = 0);
+        }
+        System.out.println(clientAccount + depositAmount + bonus);
     }
 }
